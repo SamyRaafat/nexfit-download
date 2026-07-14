db8c83e7601fd597f1ddd514fa037ba122687e4d
emamabdou777 <emamabdou777@gmail.com>
2026-07-14 19:10:31 +0300

    Web app 1.0.5: fix appbar title, hero stars overflow, remove subcat count

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "3e50a78d4fdd51db77687403c7cac3cc",
+"flutter_bootstrap.js": "aba3131be24e2f133133384624d386f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e4bb0d92ac1e6966b628d35145c739eb",
 "/": "e4bb0d92ac1e6966b628d35145c739eb",
-"main.dart.js": "d7554cc764118c3664646948f6ac3c10",
+"main.dart.js": "bc24cc54e6193b710a07c3f15a6f5372",
 "manifest.json": "14da7f0d2f103faa29b69d87c52657af",
 "version.json": "bfbe0997b0591cca61f36d89a3b39a4d"};
 // The application shell files that are downloaded before a service worker can
